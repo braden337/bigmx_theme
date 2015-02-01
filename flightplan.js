ff4eba1e98d7f89dd6edcc51972cd20ba33b8f51
braden337 <braden337@gmail.com>
2015-02-01 11:35:37 -0600

    added comments to flightplan.js and removed flightplan.js test file

--- flightplan.js
+++ flightplan.js
@@ -1,6 +1,11 @@
 var dotenv = require('dotenv');
 dotenv.load();
 
+// console.log(process.env.SERVER_HOSTNAME)
+// console.log(process.env.SERVER_USERNAME)
+// console.log(process.env.SERVER_PORT)
+// console.log(process.env.SERVER_FOLDER)
+
 var plan = require('flightplan');
 
 // configuration
