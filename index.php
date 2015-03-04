6e75fa9e793a513d95388f1953b7feb7ce7b34f8
braden337 <braden337@gmail.com>
2015-03-03 20:17:29 -0600

    changed indenting

--- index.php
+++ index.php
@@ -1,68 +1,68 @@
-        <?php get_header(); ?>
-
-          
-
-        <!-- Add your site or application content here -->
-        <div class="site-content">
-        <?php
-
-        if(have_posts()) :
-
-          while (have_posts()) : the_post();
-
-        ?>
-
-          <ul class="posts">
-
-              <li class="post no-max">
-
-                <a href="<?php the_permalink(); ?>" class="post-link post-header">
-
-                  <div class="post-line">
-                    <?php the_post_thumbnail( [56,56], $attr ); ?>
-                    <!-- <span class="show-num">150</span> -->
-                    <h2><?php the_title(); ?></h2>
-                  </div>
-
-                  <div class="meta">
-                    <svg>
-                      <use xlink:href="#icon-calendar"></use>
-                    </svg>
-                    <time datetime="<?php the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time>
-                    <svg>
-                      <use xlink:href="#icon-clock"></use>
-                    </svg>
-                    <?php the_time('g:i a'); ?>
-                  </div>
-
-                </a>
-
-              </li>
-
-
-          </ul>
+<?php get_header(); ?>
 
 
 
+<!-- Add your site or application content here -->
+<div class="site-content">
+  <?php
 
+  if(have_posts()) :
 
+    while (have_posts()) : the_post();
 
+  ?>
 
+  <ul class="posts">
 
-        <?php
+    <li class="post no-max">
 
-          endwhile;
+      <a href="<?php the_permalink(); ?>" class="post-link post-header">
 
-        else :
-
-          echo '<p>No content found</p>';
-
-        endif;
-        ?>
-
+        <div class="post-line">
+          <?php the_post_thumbnail( [56,56], $attr ); ?>
+          <!-- <span class="show-num">150</span> -->
+          <h2><?php the_title(); ?></h2>
         </div>
 
+        <div class="meta">
+          <svg>
+            <use xlink:href="#icon-calendar"></use>
+          </svg>
+          <time datetime="<?php the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time>
+          <svg>
+            <use xlink:href="#icon-clock"></use>
+          </svg>
+          <?php the_time('g:i a'); ?>
+        </div>
+
+      </a>
+
+    </li>
+
+
+  </ul>
 
 
 
-        <?php get_footer(); ?>
+
+
+
+
+
+  <?php
+
+  endwhile;
+
+  else :
+
+    echo '<p>No content found</p>';
+
+  endif;
+  ?>
+
+</div>
+
+
+
+
+<?php get_footer(); ?>
